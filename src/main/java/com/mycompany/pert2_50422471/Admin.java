2cdcd06e18d9476f6513227c6ea730a0cfe541f5
Estermei <meichikinan@gmail.com>
2025-11-25 16:25:08 +0700

    First Commit

---
+++ src/main/java/com/mycompany/pert2_50422471/Admin.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert2_50422471;
+
+/**
+ *
+ * @author meich
+ */
+public class Admin extends User{
+    
+    public void manageUser() {
+        System.out.println("Admin mengelola pengguna");
+    }
+    
+    public void login() {
+        System.out.println("Admin melakukan login");
+    }
+    
+    public void logout() {
+        System.out.println("Admin melakukan logout");
+    }
+}
---
+++ src/main/java/com/mycompany/pert2_50422471/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert2_50422471;
+
+/**
+ *
+ * @author meich
+ */
+public class User {
+    String nama;
+    String npm;
+    String email;
+    
+    public void login() {
+        System.out.println("User Melakukan Login");
+    }
+    
+    public void logout() {
+        System.out.println("User Melakukan Logout");
+    }
+}
